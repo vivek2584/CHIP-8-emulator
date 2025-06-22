672da5ba747924c3cdf7bbd2ac543732e7da5af5
vivek2584 <vivekroy2584@gmail.com>
2025-06-22 16:43:31 +0000

    limit to 700 instruction/sec

--- src/emulator_data.rs
+++ src/emulator_data.rs
@@ -11,18 +11,29 @@ pub const SPRITE_FONT_SIZE: usize = 5;
 
 pub const GPR_START: usize = 0x200;
 pub const GPR_END: usize = 0x20F;
+pub const GPR_SIZE: usize = 1;
+pub const GPR_COUNT: usize = 16;
 
 pub const STACK_START: usize = 0x211;
 pub const STACK_END: usize = 0x220;
+pub const STACK_SIZE: usize = 16;
+
+pub const STACK_PTR_LOC: usize = 0x225;
+pub const STACK_PTR_SIZE: usize = 1;
 
 pub const I_START: usize = 0x221;
 pub const I_END: usize = 0x222;
+pub const I_SIZE: usize = 2;
 
 pub const PC_START: usize = 0x223;
 pub const PC_END: usize = 0x224;
-
-pub const STACK_PTR_LOC: usize = 0x225;
+pub const PC_SIZE: usize = 2;
 
 pub const DELAY_TIMER_LOC: usize = 0x226;
+pub const DELAY_TIMER_SIZE: usize = 1;
 
 pub const SOUND_TIMER_LOC: usize = 0x227;
+pub const SOUND_TIMER_SIZE: usize = 1;
+
+pub const FREE_MEM_START: usize = 0x228;
+pub const FREE_MEM_END: usize = RAM_SIZE - 1;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use std::time::{Duration, Instant};
+
 use chip_8_emulator::*;
 
 fn main() {
     let mut ram: [u8; emulator_data::RAM_SIZE] = [0; emulator_data::RAM_SIZE];
 
     write_sprite_data(&mut ram);
+
+    let instruction_delay = Duration::from_micros(1_000_000 / 700);
+    let timer_interval = Duration::from_millis(1000 / 60);
+
+    let mut last_execution_time = Instant::now();
+    //TODO DELAY AND SOUND TIMERS delays
+
+    loop {
+        if last_execution_time.elapsed() > instruction_delay {
+            //TODO DECODE AND EXECUTE
+        }
+        last_execution_time = Instant::now();
+    }
 }
