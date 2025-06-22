365b4d49ce65d76215c5a91046c007e6bf7e3476
vivek2584 <vivekroy2584@gmail.com>
2025-06-22 17:05:04 +0000

    added todos for later

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ fn main() {
 
     write_sprite_data(&mut ram);
 
-    let instruction_delay = Duration::from_micros(1_000_000 / 700);
-    let timer_interval = Duration::from_millis(1000 / 60);
-
+    let instruction_delay = Duration::from_micros(1_000_000 / 700); // limit to 700 instructions
+                                                                    // per second
+    let timer_interval = Duration::from_millis(1000 / 60); // decrease timer 60 times per
+                                                           // second
     let mut last_execution_time = Instant::now();
     //TODO DELAY AND SOUND TIMERS delays
 
