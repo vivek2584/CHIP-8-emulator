777e378cba57b7215abebe52b69560193b1cc16c
vivek2584 <vivekroy2584@gmail.com>
2025-06-24 12:10:33 +0000

    address V0 to VF with offsets not individually

--- src/emulator_data.rs
+++ src/emulator_data.rs
@@ -9,22 +9,8 @@ pub const SPRITE_DATA_START: usize = 0x050;
 pub const SPRITE_DATA_END: usize = 0x09F;
 pub const SPRITE_FONT_SIZE: usize = 5;
 
-pub const V0: usize = 0x200;
-pub const V1: usize = 0x201;
-pub const V2: usize = 0x202;
-pub const V3: usize = 0x203;
-pub const V4: usize = 0x204;
-pub const V5: usize = 0x205;
-pub const V6: usize = 0x206;
-pub const V7: usize = 0x207;
-pub const V8: usize = 0x208;
-pub const V9: usize = 0x209;
-pub const VA: usize = 0x20A;
-pub const VB: usize = 0x20B;
-pub const VC: usize = 0x20C;
-pub const VD: usize = 0x20D;
-pub const VE: usize = 0x20E;
-pub const VF: usize = 0x20F;
+pub const GPR_START_V0: usize = 0x200;
+pub const GPR_END_VF: usize = 0x20F;
 pub const GPR_SIZE: usize = 1;
 pub const GPR_COUNT: usize = 16;
 
