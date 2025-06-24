57fe5dba1b9addff40ad13de59ffcf5b08edc4dc
vivek2584 <vivekroy2584@gmail.com>
2025-06-24 10:18:53 +0000

    add matching first 7 types of opcodes

--- src/emulator_data.rs
+++ src/emulator_data.rs
@@ -32,24 +32,24 @@ pub const STACK_START: usize = 0x210;
 pub const STACK_END: usize = 0x21F;
 pub const STACK_SIZE: usize = 16;
 
-pub const STACK_PTR_LOC: usize = 0x225;
+pub const STACK_PTR_LOC: usize = 0x224;
 pub const STACK_PTR_SIZE: usize = 1;
 
-pub const I_START: usize = 0x221;
-pub const I_END: usize = 0x222;
+pub const I_START: usize = 0x220;
+pub const I_END: usize = 0x221;
 pub const I_SIZE: usize = 2;
 
-pub const PC_START: usize = 0x223;
-pub const PC_END: usize = 0x224;
+pub const PC_START: usize = 0x222;
+pub const PC_END: usize = 0x223;
 pub const PC_SIZE: usize = 2;
 
-pub const DELAY_TIMER_LOC: usize = 0x226;
+pub const DELAY_TIMER_LOC: usize = 0x225;
 pub const DELAY_TIMER_SIZE: usize = 1;
 
-pub const SOUND_TIMER_LOC: usize = 0x227;
+pub const SOUND_TIMER_LOC: usize = 0x226;
 pub const SOUND_TIMER_SIZE: usize = 1;
 
-pub const FREE_MEM_START: usize = 0x228;
+pub const FREE_MEM_START: usize = 0x227;
 pub const FREE_MEM_END: usize = RAM_SIZE - 1;
 
 pub const INSTRUCTION_SIZE: usize = 2;
--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,14 @@ fn main() {
     let timer_interval = Duration::from_millis(1000 / 60); // decrease timer 60 times per
                                                            // second
     let mut last_execution_time = Instant::now();
-    //TODO DELAY AND SOUND TIMERS delays
+    //TODO DELAY AND SOUND TIMERS decrement at 60hz
 
     loop {
         if last_execution_time.elapsed() > instruction_delay {
             let current_pc = &ram[emulator_data::PC_START..=emulator_data::PC_END];
             let instruction_idx = u16::from_le_bytes(current_pc.try_into().unwrap()) as usize;
             update_pc(&mut ram);
-            // TODO fetch from instruction_idx the instruction and execute
+
             let instruction_as_bytes =
                 &ram[instruction_idx..instruction_idx + emulator_data::INSTRUCTION_SIZE];
 
@@ -37,6 +37,45 @@ fn main() {
                         _ => (),           //IGNORE 0x0NNN,
                     }
                 }
+
+                0x1000 => {
+                    todo!() // TODO EXTRACT NNN FROM 1NNN AND SET PC TO NNN, DO NOT INCREMENT PC
+                }
+
+                0x2000 => {
+                    todo!() // TODO SAME AS 1NNN but first push current pc value to stack to return
+                            // with 00EE
+                }
+
+                0x3000 => {
+                    todo!() //TODO 3XNN =>  CHECK IF VX IS EQUAL TO NN THEN SKIP ONE INSTRUCTION
+                            //TODO MAKE A FUNCTION TO MAP THE EXTRACTED X VALUE TO REGISTER VX
+                }
+
+                0x4000 => {
+                    todo!() //TODO 4XNN => SAME AS ABOVE BUT VX SHOULD NOT BE EQUAL TO NN
+                }
+
+                0x5000 => {
+                    todo!() // 5XY0 => CHECK IF VALUES IN VX AND VY ARE EQUAL AND SKIP ONE
+                            // INSTRUCTION
+                }
+
+                0x6000 => {
+                    todo!() // 6XNN => SET VX TO NN
+                }
+
+                0x7000 => {
+                    todo!() // 7XNN => ADD NN TO VX and wrap around if overflow
+                }
+
+                0x8000 => {}
+
+                0x9000 => {
+                    todo!() // 9XY0 => CHECK IF VALUES IN VX AND VY ARE NOT EQUAL AND SKIP ONE
+                            // INSTRUCTION
+                }
+
                 _ => todo!(),
             }
 
